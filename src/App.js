2984626fa65eb37d5745fbfb3222385bea384931
danihurfar <dahurfar@gmail.com>
2022-05-17 14:56:10 -0400

    cambio del mensaje

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React, {useState} from 'react';
-import styled from 'styled-components';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-    faXmark
-  } from "@fortawesome/free-solid-svg-icons";
+import Modal from './componentes/Modal'
 import './index.css';
 
 
@@ -11,92 +7,27 @@ const App = () => {
 	const [estadoModalGoGreen, cambiarEstadoGoGreen] = useState(true);
 	return (
 		<div>
-			
+			<Modal 
+			estadoGoGreen={estadoModalGoGreen}
+			cambiarGoGreen={cambiarEstadoGoGreen}
+			>	
 
-			{estadoModalGoGreen &&
-                <Overlay>
-                    <ContenedorModal>
-                        <div className="container mt-3">
-                            <div className="row ">
-                                <div className="col">
-                                    <div className="mt-1 mb-3 lb-text-center lb-w-60 mx-auto lb-go-green-message">
-                                        <span className="">
-                                            Hi everyone!!! How are you? <br />
-                                            <br />
-                                            HELP US GO GREEN ♻️, no more paper 📝 <br />
-                                            <br />
-                                            We’ve been trying for a few months now to go green ♻️and
-                                            we’ve come a long way but we need your help to go all the
-                                            way. <br />
-                                            <br />
-                                            Please no more paper!❌ <br />
-                                            <br />
-                                            Please fill all the information that the system requires for
-                                            your check in <br />
-                                            <br />
-                                            ♻️THINK GREEN, THINK CLEAN ♻️
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <BotonCerrar onClick={() => cambiarEstadoGoGreen(false)}>
-                           <FontAwesomeIcon icon={faXmark}> </FontAwesomeIcon>
-                        </BotonCerrar>
-                    </ContenedorModal>
-                </Overlay>
-            }
 			
+			</Modal>
 
-		
+			<h1>What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+Why do we use it?
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
+
+Where does it come from?
+Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</h1>
 		</div>
 	);
 }
  
 export default App;
-
-const Overlay = styled.div`
-    width: 100vw;
-    height: 100vh;
-    position: fixed;
-    top: 0;
-    left: 0;
-    background: rgba(0,0,0,.5) ;
-    padding: 40 px;
-    display: flex;
-    align-items: center ;
-    justify-content: center;
-    `;
-
-const ContenedorModal = styled.div`
-    width: 500px;
-    min-height: 100px;
-    background: #C2608E;
-    position: relative;
-    border-radius: 5px;
-    box-shadow: rgba(100,100,111, 0.2) 0px 7px 29px 0px;
-    padding: 20px;
-`;
-
-const BotonCerrar = styled.div`
-    position: absolute;
-    top: 15px;
-    right: 20px;
-
-    width: 30px;
-    height: 30px;
-    background: none;
-    cursor: pointer;
-    transition: .3s ease all;
-    border-radius: 5px;
-    color: #000000;
-
-    &:hover {
-        background: #f2f2f2;
-    }
-
-    svg {
-        width: 100%;
-        height: 100%;
-    }
-`;
